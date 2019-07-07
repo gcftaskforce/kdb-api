e329e19f2abc39f4f8704cc44281e1b128a4ad54
Darren Anderson <synchronopeia@gmail.com>
2019-07-06 21:55:00 -0700

    use Datastore directly

--- check.js
+++ check.js
@@ -3,9 +3,25 @@
 require('dotenv').config();
 const debug = require('debug')('api:check');
 
-const models = require('./models');
+const { Datastore } = require('@google-cloud/datastore');
 
-(async (myVal) => {
-  const entities = await models.value.dump();
-  debug(entities);
+const ds = new Datastore();
+
+// const models = require('./models');
+
+(async () => {
+  // const entities = await models.value.dump();
+  // debug(entities);
+  const dsQuery = ds.createQuery('Value');
+  let entities = [];
+  try {
+    const results = await ds.runQuery(dsQuery);
+    if (results[0].length) {
+      [entities] = results;
+    }
+  } catch (err) {
+    throw err;
+  }
+  // debug(entities);
+  return entities;
 })();
